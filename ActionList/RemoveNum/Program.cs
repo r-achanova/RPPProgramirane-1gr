f266bcada50089abc50daf60d3efc99fdb62c793
r-achanova <r.achanova@pmg-pernik.com>
2023-02-15 09:29:51 +0200

    add task1

---
+++ ActionList/RemoveNum/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RemoveNum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> nums = Console.ReadLine().Split().Select(int.Parse).ToList();
+            int last = nums[nums.Count - 1];//last element
+            int last1 = nums.Last(); //last element
+           //  nums.RemoveAll(x => x == last);
+            for (int i = 0; i < nums.Count; i++)
+            {
+                if (nums[i]==last)
+                {
+                    nums.RemoveAt(i);
+                    i--;
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", nums));
+        }
+    }
+}
